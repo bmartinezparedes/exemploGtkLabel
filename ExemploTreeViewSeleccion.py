77239c5c9c3ed1e04f87a4cfccf42eca0842d176
Braiskiskos <bmartinezparedes@danielcastelao.com>
2022-01-17 14:40:09 +0100

    TreeView con seleccion 2 17/1/2022

--- ExemploTreeViewSeleccion.py
+++ ExemploTreeViewSeleccion.py
@@ -32,21 +32,43 @@ class Aplicacion(Gtk.Window):
             columna = Gtk.TreeViewColumn(tituloColumna, celda, text=i)
             trvVistaProgramas.append_column(columna)
 
+        seleccion = trvVistaProgramas.get_selection()
+        seleccion.connect("changed", self.on_trvVistaProgramas_changed)
+
         scroll = Gtk.ScrolledWindow()
         scroll.set_vexpand(True)
         grid.attach(scroll, 0, 0, 8, 10)
         scroll.add(trvVistaProgramas)
 
-        seleccion = trvVistaProgramas.get_selection()
-        seleccion.connect("changed", self.on_trvVistaProgramas_changed)
+        self.sofware = Gtk.Entry()
+        self.ano = Gtk.Entry()
+        self.linProgramacion = Gtk.Entry()
+        self.modificar = Gtk.Button(label="Modificar")
+        self.modificar.connect("clicked", self.on_modificar_clicked, seleccion)
+        self.modificar.set_sensitive(False)
+
+        grid.attach(self.sofware,0,10,2,1)
+        grid.attach_next_to(self.ano,self.sofware,Gtk.PositionType.RIGHT,2,1)
+        grid.attach_next_to(self.linProgramacion, self.ano, Gtk.PositionType.RIGHT, 2, 1)
+        grid.attach_next_to(self.modificar,self.linProgramacion,Gtk.PositionType.RIGHT, 2, 1)
 
         self.add(grid)
         self.connect("destroy", Gtk.main_quit)
         self.show_all()
+
     def on_trvVistaProgramas_changed(self,selec):
         modelo, punteiro = selec.get_selected()
-        if punteiro is not None:
-            print("Seleccionaches ",modelo[punteiro][0])
+        self.sofware.set_text(modelo[punteiro][0])
+        self.ano.set_text(str(modelo[punteiro][1]))
+        self.linProgramacion.set_text(modelo[punteiro][2])
+        self.modificar.set_sensitive(True)
+
+    def on_modificar_clicked(self,control,seleccion):
+        modelo, fila = seleccion.get_selected()
+        modelo[fila][0] = self.sofware.get_text()
+        modelo[fila][1] = int(self.ano.get_text())
+        modelo[fila][2] = self.linProgramacion.get_text()
+        self.modificar.set_sensitive(False)
 
 if __name__=="__main__":
     Aplicacion()
