4bd6b528e976b7e517dd6f2e92a96b4bb9673b9e
Braiskiskos <bmartinezparedes@danielcastelao.org>
2022-01-19 11:04:00 +0100

    TreeViewOrdenado y Filtrado (Filrtrado echo) 19/01/2022

--- ExemploTreeViewOrdenadoFiltrado.py
+++ ExemploTreeViewOrdenadoFiltrado.py
@@ -14,6 +14,8 @@ class Aplicacion(Gtk.Window):
         self.set_border_width(5)
         self.set_default_size(400,200)
 
+        self.filtro_actual_linguaxe = "None"
+
         programas = Gtk.ListStore(str, int, str)
         programas.append(["FireFox",1960, "C++"])
         programas.append(["Eclipse",1999, "Java"])
@@ -22,11 +24,17 @@ class Aplicacion(Gtk.Window):
         programas.append(["VisualEstudio",2005, "PHP"])
         programas.append(["Chrome",2022, "C"])
         programas.append(["Netbeans",1998, "Java"])
+        programas.append(["kernel Linux",1991,"C"])
+        programas.append(["Sphinx",2003,"Python"])
+
+        self.filtroLinguaxe = programas.filter_new()
+        self.filtroLinguaxe.set_visible_func(self.filtro_linguaxe)
+
 
         grid = Gtk.Grid()
         grid.set_column_homogeneous(True)
         grid.set_row_homogeneous(True)
-        trvVistaProgramas = Gtk.TreeView(model=programas)
+        trvVistaProgramas = Gtk.TreeView(model=self.filtroLinguaxe)
         for i, tituloColumna in enumerate (["Software","Ano","Linguaxe de programación"]):
             celda = Gtk.CellRendererText()
             columna = Gtk.TreeViewColumn(tituloColumna, celda, text=i)
@@ -37,9 +45,37 @@ class Aplicacion(Gtk.Window):
         grid.attach(scroll, 0, 0, 8, 10)
         scroll.add(trvVistaProgramas)
 
+        filtroJava= Gtk.Button (label="Java")
+        filtroC = Gtk.Button(label="C")
+        filtroCMaisMais = Gtk.Button(label="C++")
+        filtroPython = Gtk.Button(label="Python")
+        senFiltro = Gtk.Button(label="None")
+
+        filtroJava.connect("clicked", self.on_botonSeleccion_clicked)
+        filtroC.connect("clicked", self.on_botonSeleccion_clicked)
+        filtroPython.connect("clicked", self.on_botonSeleccion_clicked)
+        filtroCMaisMais.connect("clicked", self.on_botonSeleccion_clicked)
+        senFiltro.connect("clicked", self.on_botonSeleccion_clicked)
+
+        grid.attach_next_to(filtroJava,scroll,Gtk.PositionType.BOTTOM,1,1)
+        grid.attach_next_to(filtroC, filtroJava, Gtk.PositionType.RIGHT, 1, 1)
+        grid.attach_next_to(filtroCMaisMais, filtroC, Gtk.PositionType.RIGHT, 1, 1)
+        grid.attach_next_to(filtroPython, filtroCMaisMais, Gtk.PositionType.RIGHT, 1, 1)
+        grid.attach_next_to(senFiltro, filtroPython, Gtk.PositionType.RIGHT, 1, 1)
+
         self.add(grid)
         self.connect("destroy", Gtk.main_quit)
         self.show_all()
+    def on_botonSeleccion_clicked(self,boton):
+        self.filtro_actual_linguaxe=boton.get_label()
+        self.filtroLinguaxe.refilter()
+
+    def filtro_linguaxe(self,modelo,fila,datos):
+        if(self.filtro_actual_linguaxe=="None"):
+            return True
+        else:
+            return modelo[fila][2]== self.filtro_actual_linguaxe
+
 
 if __name__=="__main__":
     Aplicacion()
---
+++ ejercicio.py
@@ -0,0 +1,34 @@
+import gi
+
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
+
+class Aplicacion(Gtk.Window):
+    def __init__(self):
+        super().__init__()
+        self.set_title("Ejercicio")
+
+        notebook = Gtk.Notebook()
+        self.add(notebook)
+
+        cajaV1= Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        notebook.append_page(cajaV1,Gtk.Label(label="MenuWidget1"))
+        cajaV2 = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        notebook.append_page(cajaV2,Gtk.Label(label="MenuWidget2"))
+
+        cajaH =Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+        boton = Gtk.Button(label="ToolbarButton")
+        cajaH.add(boton)
+        checkButon = Gtk.CheckButton(label="ToolbarCheckButton")
+        cajaH.add(checkButon)
+        cajaV1.add(cajaH)
+        lblPanelCaption = Gtk.Label(label="Panel Caption")
+        cajaV1.pack_start(lblPanelCaption)
+
+        self.connect("destroy", Gtk.main_quit)
+        self.show_all()
+
+if __name__ == "__main__":
+    Aplicacion()
+    Gtk.main()
