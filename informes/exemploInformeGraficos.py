18763381e9abd097ae9b572e38e7bd8cb8fab1eb
Braiskiskos <bmartinezparedes@danielcastelao.org>
2022-02-09 11:52:05 +0100

    9/2/2022 lenda a la grafica

--- informes/exemploInformeGraficos.py
+++ informes/exemploInformeGraficos.py
@@ -2,6 +2,7 @@ from reportlab.platypus import SimpleDocTemplate, Spacer
 from reportlab.graphics.charts.piecharts import Pie
 from reportlab.graphics.charts.barcharts import VerticalBarChart
 from reportlab.graphics.charts.textlabels import Label
+from reportlab.graphics.charts.legends import Legend
 from reportlab.graphics.shapes import Drawing
 from reportlab.lib.pagesizes import A4
 from reportlab.lib import colors
@@ -68,6 +69,36 @@ tarta.slices[3].fontColor = colors.red
 tarta.sideLabels = 1
 
 d.add(tarta)
+
+lenda = Legend()
+lenda.x = 370
+lenda.y = 0
+lenda.dx = 8
+lenda.dy = 8
+lenda.fontName = 'Helvetica'
+lenda.fontSize = 7
+lenda.boxAnchor = 'n'
+lenda.columnMaximum = 10
+lenda.strokeWidth = 1
+lenda.strokeColor = colors.black
+lenda.autoXPadding = 5
+lenda.yGap = 0
+lenda.dxTextSpace = 5
+lenda.alignment = 'right'
+lenda.dividerLines = 2|3|4
+lenda.dividerOffsY = 4.5
+lenda.subCols.rpad = 30
+lenda.deltax = 75
+lenda.deltay = 10
+
+colores = [colors.pink, colors.red, colors.green, colors.blue, colors.yellow]
+for i, color in enumerate(colores):
+       tarta.slices[i].fillColor = color
+
+lenda.colorNamePairs = [(tarta.slices[i].fillColor, (tarta.labels[i][0:20], '%0.2f' % tarta.data[i])
+                         ) for  i in range (len(tarta.data))]
+
+d.add(lenda)
 guion.append(d)
 
 doc = SimpleDocTemplate("informeGraficos.pdf", pagesize = A4)
